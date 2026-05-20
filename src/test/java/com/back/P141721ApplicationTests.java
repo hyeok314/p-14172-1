84a170a54d90c96f33ef4ac476d446989ccc7dfe
hyeok314 <joysys8272@gmail.com>
2026-05-20 09:07:20 +0900

    git init

---
+++ src/test/java/com/back/P141721ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class P141721ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
